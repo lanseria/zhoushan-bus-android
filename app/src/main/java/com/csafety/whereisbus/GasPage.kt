c19297893895e92e88741079b90aff091133996d
lanseria <zhangchao564265135@hotmail.com>
2023-08-10 18:57:07 +0800

    FIX gas interface

--- app/src/main/java/com/csafety/whereisbus/GasPage.kt
+++ app/src/main/java/com/csafety/whereisbus/GasPage.kt
@@ -7,9 +7,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -19,12 +16,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.csafety.whereisbus.ui.theme.WhereIsBusTheme
 
 
 @Composable
-fun GasPage (data: BusPagesData, title: String = "油价查询") {
+fun GasPage(data: BusPagesData, title: String = "油价查询") {
     val gasPriceList = data.gasPrice
     val profile = remember {
         mutableStateOf(gasPriceList)
@@ -44,37 +40,35 @@ fun GasPage (data: BusPagesData, title: String = "油价查询") {
         TitleBar(title)
         Row(
             horizontalArrangement = Arrangement.SpaceEvenly
-        ){
-            gasTypeList.forEach {
-                 item ->
+        ) {
+            gasTypeList.forEach { item ->
                 Text(item.label, Modifier.weight(1f))
             }
         }
-    }
-    LazyColumn {
-        itemsIndexed(
-            items = profile.value
-        ) {
-            _, gas ->
-            Row(
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ){
-            gasTypeList.forEach {
-                       item ->
-                    if (item.label == "province")
-                        Text(gas.province,Modifier.weight(1f))
-                    if (item.label == "gasoline92")
-                        Text(gas.gasoline92,Modifier.weight(1f))
-                    if (item.label == "gasoline95")
-                        Text(gas.gasoline95,Modifier.weight(1f))
-                    if (item.label == "gasoline98")
-                        Text(gas.gasoline98,Modifier.weight(1f))
-                    if (item.label == "diesel")
-                        Text(gas.diesel,Modifier.weight(1f))
+        LazyColumn {
+            itemsIndexed(
+                items = profile.value
+            ) { _, gas ->
+                Row(
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    gasTypeList.forEach { item ->
+                        if (item.value == "province")
+                            Text(gas.province, Modifier.weight(1f))
+                        if (item.value == "gasoline92")
+                            Text(gas.gasoline92, Modifier.weight(1f))
+                        if (item.value == "gasoline95")
+                            Text(gas.gasoline95, Modifier.weight(1f))
+                        if (item.value == "gasoline98")
+                            Text(gas.gasoline98, Modifier.weight(1f))
+                        if (item.value == "diesel")
+                            Text(gas.diesel, Modifier.weight(1f))
+                    }
                 }
             }
         }
     }
+
 }
 
 
